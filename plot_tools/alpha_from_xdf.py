716208fad279af4e38445a193e8cf6f38ae21dfa
Alexander Putilin <avp-13@yandex.ru>
2024-03-22 22:51:50 +0000

    Fix db fitting

--- plot_tools/alpha_from_xdf.py
+++ plot_tools/alpha_from_xdf.py
@@ -19,29 +19,20 @@ def one_over_f(freq, alpha, beta):
 raw = load_raw_xdf(sys.argv[1])
 filter_and_drop_dead_channels(raw)
 
-psd = raw.compute_psd(fmax=60.0)
+psd = raw.compute_psd(fmin=1.0, fmax=45.0)
 peak_alpha_freq = get_peak_alpha_freq(psd)
 
 psd_values, psd_freqs = raw.compute_psd(fmin=1.0, fmax=45.0).get_data(return_freqs=True)
 
-psd_values_db = 10 * np.log10(psd_values)
+psd_values_db = 10 * np.log10(psd_values * 1e6 * 1e6) 
 psd_mean = np.mean(psd_values_db, axis=0)
 
 
 fit_freq_range = (psd_freqs > 3) & (psd_freqs < 20)  # Adjust the range as needed
 
-psd_mean = psd_mean - np.mean(psd_mean)
-# TODO XXX: I'm not sure why this is needed but the fit is better with it
-psd_mean = psd_mean + np.mean(psd_mean[fit_freq_range])  
-
-print("Fit data", psd_freqs[fit_freq_range], psd_mean)
 popt, _ = curve_fit(one_over_f, psd_freqs[fit_freq_range], psd_mean[fit_freq_range])
 alpha, beta = popt
-
-print("Fitted alpha and beta:", alpha, beta)
-
 fitted_curve = one_over_f(psd_freqs[fit_freq_range], alpha, beta)
-
 
 
 fig = psd.plot(average=True, show=False)
