673b11e081ffeb256387dace7aa5ea49d63c799e
Alexander Putilin <avp-13@yandex.ru>
2024-04-01 16:45:30 +0100

    Refactor curve fitting in a separate function

--- plot_tools/alpha_from_xdf.py
+++ plot_tools/alpha_from_xdf.py
@@ -1,19 +1,13 @@
 import sys 
 
-import mne.time_frequency
 import numpy as np
-from scipy.stats import linregress
-from scipy.optimize import curve_fit
 
 import matplotlib.pyplot as plt
 
 from libs.file_formats import load_raw_xdf
 from libs.filters import filter_and_drop_dead_channels
 from libs.plot import add_red_line_with_value
-from libs.psd import get_peak_alpha_freq
-
-def one_over_f(freq, alpha, beta):
-    return alpha / freq ** beta
+from libs.psd import get_peak_alpha_freq, fit_one_over_f_curve
 
 
 raw = load_raw_xdf(sys.argv[1])
@@ -22,25 +16,13 @@ filter_and_drop_dead_channels(raw)
 psd = raw.compute_psd(fmin=1.0, fmax=60.0)
 peak_alpha_freq = get_peak_alpha_freq(psd)
 
-psd_values, psd_freqs = psd.get_data(return_freqs=True)
-
-psd_values_db = 10 * np.log10(psd_values * 1e6 * 1e6) 
-psd_mean = np.mean(psd_values_db, axis=0)
-
-
-fit_freq_range = (psd_freqs > 3) & (psd_freqs < 40)  # Adjust the range as needed
-
-popt, _ = curve_fit(one_over_f, psd_freqs[fit_freq_range], psd_mean[fit_freq_range])
-
-alpha, beta = popt
-fitted_curve = one_over_f(psd_freqs[fit_freq_range], alpha, beta)
-
+psd_freqs, fit_freq_range, fitted_curve, delta_db = fit_one_over_f_curve(psd, min_freq=3, max_freq=40, peak_alpha_freq=peak_alpha_freq)
 
 fig = psd.plot(average=True, show=False)
 ax = fig.get_axes()[0]
 ax.plot(psd_freqs[fit_freq_range], fitted_curve, label='1/f fit', linewidth=1, color='darkmagenta')
 
-delta_db = psd_mean[np.where(psd_freqs == peak_alpha_freq)][0] - one_over_f(peak_alpha_freq, alpha, beta)
+
 add_red_line_with_value(fig, peak_alpha_freq, delta_db)
 
 plt.show()
