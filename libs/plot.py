d8db88e6c3412105c670169987c483450865787e
Alexander Putilin <avp-13@yandex.ru>
2024-04-09 00:04:52 +0100

    Fix the legend of the PSD plot with separate channels not showing up

--- libs/plot.py
+++ libs/plot.py
@@ -3,22 +3,23 @@ import matplotlib
 from libs.psd import fit_one_over_f_curve, get_peak_alpha_freq
 
 def add_red_line_with_value(fig, value, delta_db):
-    for ax in fig.axes:
-        ax.axvline(x=value, color='red', linestyle='-', linewidth=1.0)
+    ax = fig.get_axes()[0]
 
-        y_min, y_max = ax.get_ylim()
-        y_shift = (y_max - y_min) * 0.05  # Adjust the multiplication factor as needed
+    ax.axvline(x=value, color='red', linestyle='-', linewidth=1.0)
 
-        if delta_db is not None:
-            text = f'{value:.2f} Hz, {delta_db:.2f} dB'
-        else:
-            text = f'{value:.2f} Hz'
+    y_min, y_max = ax.get_ylim()
+    y_shift = (y_max - y_min) * 0.05  # Adjust the multiplication factor as needed
 
-        offset = matplotlib.transforms.ScaledTranslation(2/72, 0, fig.dpi_scale_trans)
-        text_transform = ax.transData + offset
+    if delta_db is not None:
+        text = f'{value:.2f} Hz, {delta_db:.2f} dB'
+    else:
+        text = f'{value:.2f} Hz'
 
-        ax.text(value, y_max - y_shift, text,
-                ha='left', va='top', color='red', fontsize=8, transform=text_transform)
+    offset = matplotlib.transforms.ScaledTranslation(2/72, 0, fig.dpi_scale_trans)
+    text_transform = ax.transData + offset
+
+    ax.text(value, y_max - y_shift, text,
+            ha='left', va='top', color='red', fontsize=8, transform=text_transform)
 
 
 def plot_psd(psd, title=None, average=True):
@@ -26,10 +27,13 @@ def plot_psd(psd, title=None, average=True):
     psd_freqs, fit_freq_range, fitted_curve, delta_db = fit_one_over_f_curve(psd, min_freq=3, max_freq=40, peak_alpha_freq=peak_alpha_freq)
 
     fig = psd.plot(average=average, show=False, spatial_colors=True)
+
     ax = fig.get_axes()[0]
     ax.plot(psd_freqs[fit_freq_range], fitted_curve, label='1/f fit', linewidth=1, color='darkmagenta')
     
     if title is not None:
         ax.set_title(title)
+
+
 
     add_red_line_with_value(fig, peak_alpha_freq, delta_db)
