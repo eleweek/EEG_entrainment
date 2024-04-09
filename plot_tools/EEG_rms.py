40818a8cdf5e1e9b18d7cd1dd4a337158038e8ed
Alexander Putilin <avp-13@yandex.ru>
2024-04-10 00:42:22 +0100

    Get sensor names and actually display something resembling uvrms in real time

--- plot_tools/EEG_rms.py
+++ plot_tools/EEG_rms.py
@@ -1,9 +1,12 @@
 # EEG_rms.py
+import time
 
-import pygame, time
+import numpy as np
+
+import pygame
 from pylsl import StreamInfo, StreamOutlet, StreamInlet, resolve_stream
 import mne
-
+from libs.stats import fake_uvrms
 
 print("looking for an EEG stream...")
 streams = resolve_stream('type', 'EEG')
@@ -11,7 +14,20 @@ stream_idx = 0
 inlet = StreamInlet(streams[stream_idx])
 EEG_sample, timestamp = inlet.pull_chunk(max_samples=250)
 
-channel_count = inlet.info().channel_count()
+
+info = inlet.info()
+channel_count = info.channel_count()
+xml_desc = info.desc()
+sensor_names = []
+ch = xml_desc.child("channels").child("channel")
+while not ch.empty():
+    # Assuming that the label is stored in a child element named 'label'
+    label = ch.child_value("label")
+    if label:
+        sensor_names.append(label)
+    ch = ch.next_sibling("channel")
+
+print(f"Found {channel_count} channels: {', '.join(sensor_names)}")
 
 
 device_info = f"Device {stream_idx}: {streams[stream_idx].name()}"
@@ -57,11 +73,12 @@ while t<t_n:
          # Get latest second of EEG
         EEG_sample, timestamp = inlet.pull_chunk(max_samples=250)
 
-        ch_names = [f'EEG {ch}' for ch in range(channel_count)]
-        ch_types = ['eeg'] * channel_count
-        info = mne.create_info(ch_names=ch_names, sfreq=250, ch_types=ch_types)
+        print("EEG data shape", np.asarray(EEG_sample).T.shape)
 
-        raw = mne.io.RawArray(EEG_sample, info)
+        ch_types = ['eeg'] * channel_count
+        info = mne.create_info(ch_names=sensor_names, sfreq=250, ch_types=ch_types)
+
+        raw = mne.io.RawArray(np.asarray(EEG_sample).T / 1e6, info)
 
         # TODO
         # Actually implement RMS
@@ -74,7 +91,7 @@ while t<t_n:
         pygame.event.get()
         screen.fill(wht)
         screen.blit(device_info_text,device_info_rect)
-        trial_text = f"Most recent RMS: {fake_RMS.join(', ')}"
+        trial_text = f"Most recent RMS: {','.join(str(d) for d in fake_uvrms(raw.get_data()).tolist())}"
         text = font.render(trial_text, True, gry) 
         screen.blit(text, text_rect)
         pygame.display.flip()
