c89bbcd8eb1e359ec6fb09207aaa56fe57a13bfa
Alexander Putilin <avp-13@yandex.ru>
2024-04-11 22:44:12 +0100

    Use pygame in EEG_rms2

--- plot_tools/EEG_rms2.py
+++ plot_tools/EEG_rms2.py
@@ -2,6 +2,8 @@ import time
 
 import numpy as np
 import mne
+import pygame
+
 from mne_lsl.lsl import resolve_streams, StreamInlet
 
 from libs.filters import filter_and_drop_dead_channels
@@ -30,14 +32,28 @@ print(f"Found {n_channels} channels", names)
 print("Sampling rate:", sampling_rate)
 print("Units:", stream_info.get_channel_units())
 
-
 all_data = []
+max_seconds = 20
+
+
+pygame.init()
+pygame.display.init() 
+screen_width, screen_height = 1000, 1000
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('EEG Noise RMS Display')
+font = pygame.font.Font(None, 36)
+
+# colors
+gry = (128,128,128)
+wht = (255,255,255)
+
 
 while True:
     # Pull data from the LSL stream
     data, timestamps = inlet.pull_chunk()
 
-    all_data.extend(data)
+    all_data.extend(data)[:-int(sampling_rate) * max_seconds]
 
     if len(data) > 0:
         # Convert data to a NumPy array
@@ -53,7 +69,16 @@ while True:
 
         uvrms = np.sqrt(np.mean(last_second_data ** 2, axis=1))
 
+        # Update screen
+        pygame.event.get()
+        screen.fill(wht)
+        trial_text = f"Most recent RMS: {','.join(str(d) for d in uvrms(raw.get_data()).tolist())}"
+        text = font.render(trial_text, True, gry) 
+        screen.blit(text, text.get_rect(center=(screen_width/2, screen_height/2)))
+        pygame.display.flip()
+
         print("uVRMS:", uvrms)
+        
 
     
     time.sleep(0.3)
