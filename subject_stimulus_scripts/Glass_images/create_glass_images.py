b56754588641e6a523a3176b36cabf6d6f387a2d
Andrew X Stewart <5137405+andrewxstewart@users.noreply.github.com>
2024-04-18 14:59:58 -0700

    Spiral Glass images

--- subject_stimulus_scripts/Glass_images/create_glass_images.py
+++ subject_stimulus_scripts/Glass_images/create_glass_images.py
@@ -10,6 +10,18 @@ import matplotlib.pyplot as plt
 width = 500
 height = 500
 
+# Glass params
+circ_here = 1
+initial_dot_density = 0.03
+dot_offset_px = 6 # Somewhere around 4-8 pixels for 16 arc-min
+if circ_here:
+    spiral_angle = 90  # 
+else:
+    # For radial stim, dot twin should be straight line from center
+    spiral_angle = 0
+
+
+
 # Create a blank image
 image = np.zeros((width, height, 3), dtype=np.uint8)
 
@@ -19,8 +31,10 @@ glass_color = (127, 127, 127)
 # Draw dots on the image
 for i in range(width):
     for j in range(height):
-        if random.random() < 0.5:
+        if random.random() < initial_dot_density:
             image[i, j] = glass_color
+
+            
 
 # Display the image
 plt.imshow(image)
