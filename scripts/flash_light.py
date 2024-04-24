8d1551e2e1afcbde520a828592a1dce26fce8d54
Alexander Putilin <avp-13@yandex.ru>
2024-04-24 18:00:24 +0100

    Make flashes reasonably short

--- scripts/flash_light.py
+++ scripts/flash_light.py
@@ -9,7 +9,7 @@ width, height = 400, 400
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Flashing Square")
 
-frequency = 10
+frequency = 8
 
 white = (255, 255, 255)
 black = (0, 0, 0)
@@ -22,15 +22,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    flash_duration = 1 / (2 * frequency)
-
-    pygame.draw.rect(screen, black, (150, 150, 100, 100))
-    pygame.display.flip()
-    time.sleep(flash_duration)
+    start_flash_time = time.time()
+    flash_duration = 1 / 30.0
 
     pygame.draw.rect(screen, white, (150, 150, 100, 100))
     pygame.display.flip()
     time.sleep(flash_duration)
+
+    pygame.draw.rect(screen, black, (150, 150, 100, 100))
+    pygame.display.flip()
+    time.sleep(1.0 / frequency - time.time() + start_flash_time)
 
 # Quit Pygame
 pygame.quit()
