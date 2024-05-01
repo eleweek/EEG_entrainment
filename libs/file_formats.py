6b991a67bc9070cdb05c4aa90bb22c7ba751b146
Alexander Putilin <avp-13@yandex.ru>
2024-05-01 22:46:40 +0100

    Add code for reading muse CSV data

--- libs/file_formats.py
+++ libs/file_formats.py
@@ -1,6 +1,50 @@
+import math
+
 import mne
 import numpy as np
+import pandas as pd
 import pyxdf
+
+# A function to load data in Daniel Ingram's format here: https://osf.io/srfnz/
+# Might not work for Muse data coming from other sources
+def load_muse_csv(file_path):
+    def estimate_sfreq(timestamps):
+        """Estimate sampling frequency by bucketing data for each second."""
+        bucket_counts = timestamps.dt.floor('s').value_counts()
+
+        avg_count = bucket_counts.mean()
+        sfreq = math.ceil(avg_count)
+
+        return sfreq
+
+    # Define the schema for the CSV file
+    schema = {
+        'TimeStamp': str,
+        'RAW_TP9': float,
+        'RAW_AF7': float,
+        'RAW_AF8': float,
+        'RAW_TP10': float
+    }
+
+    # Read the CSV file with the specified schema
+    df = pd.read_csv(file_path, dtype=schema)
+
+    ch_names = ['TP9', 'AF7', 'AF8', 'TP10']
+    data = df[['RAW_' + name for name in ch_names]].values.T
+
+    # Estimate sampling frequency from timestamps
+    timestamps = df['TimeStamp'].str.slice(0, 24)  # Truncate the timestamps
+    timestamps = pd.to_datetime(timestamps, format='%Y-%m-%d %H:%M:%S.%f')
+
+
+    sfreq = estimate_sfreq(timestamps)
+    assert sfreq == 256, f'Unexpected sampling frequency: {sfreq}'
+
+    info = mne.create_info(ch_names=ch_names, sfreq=sfreq, ch_types=['eeg'] * len(ch_names), verbose=False)
+    raw = mne.io.RawArray(data / 1e6, info, verbose=False)
+
+    return raw
+
 
 def load_openbci_txt(file_path):
     # TODO: unhardcode the sampling frequency and channel names
@@ -60,6 +104,8 @@ def load_raw_xdf(file_path):
 def load_recording(file_path):
     if file_path.endswith('.txt'):
         return load_openbci_txt(file_path)
+    elif file_path.endswith('.csv'):
+        return load_muse_csv(file_path)
     elif file_path.endswith('.xdf'):
         return load_raw_xdf(file_path)
     elif file_path.endswith('.vhdr'):
